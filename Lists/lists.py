f0174ea88ac7bab7cebc7ee5d4e3db74f015a10e
Arnold Chris <arnoldchrisoduor@gmail.com>
2023-10-22 23:54:07 +0300

    lists practice

---
+++ Lists/lists.py
@@ -0,0 +1,15 @@
+List = [1, 2, 3, 4, "ghp", 4.5]
+print("\nList containing multiple values: ")
+print(List)
+
+List2 = [["geeks", "dorks"],[1, 34, 78]]
+print("\nMulti-dimensional lists")
+print(List2)
+
+print("\nAccessing items form the single dimensional list")
+print(List[0])
+print(List[-1])
+
+print("Accessing items from the multidimensional list")
+print(List2[0][1])
+print(List2[1][1])
