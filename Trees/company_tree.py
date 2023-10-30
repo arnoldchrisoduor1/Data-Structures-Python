4231ea8acde2e4f259352ab74b1013446fdddef3
Arnold Chris <arnoldchrisoduor@gmail.com>
2023-10-30 20:01:59 +0300

    company tree updates

--- Trees/company_tree.py
+++ Trees/company_tree.py
@@ -12,15 +12,47 @@ class TreeNode:
             level += 1
             p = p.parent
         return level
+    
     def add_child(self, child):
         child.parent = self
         self.children.append(child)
 
+    def print_tree(self, property_name):
+        if property_name == 'both':
+            value = self.name + "(" + self.designation + ")"
+        elif property_name == 'name':
+            value = self.name
+        else:
+            value = self.designation
+
+        spaces = ' ' * self.get_level() * 3
+        prefix = spaces + "|__" if self.parent else ""
+        print(prefix + value)
+        if self.children:
+            for child in self.children:
+                child.print_tree(property_name)
+
 def build_management_tree():
     #CTO hierarchy
     infra_head = TreeNode("Vishwa", "Infrastructure Head")
-    infra_head.add.child(TreeNode("Dhaval", "Cloud Manager"))
-    infra_head.add.child(TreeNode("Abhijit", "Cloud Manager"))
+    infra_head.add_child(TreeNode("Dhaval", "Cloud Manager"))
+    infra_head.add_child(TreeNode("Abhijit", "Cloud Manager"))
+
+    cto = TreeNode("Chinmay", "CTO")
+    cto.add_child(infra_head)
+    cto.add_child(TreeNode("Aamir", "Application Head"))
+
+    # HR hierarchy
+    hr_head = TreeNode("Gels", "HR Head")
+
+    hr_head.add_child(TreeNode("Peter", "Recruitment Manager"))
+    hr_head.add_child(TreeNode("Waqas", "Policy Manager"))
+
+    ceo = TreeNode("Nilpul", "CEO")
+    ceo.add_child(cto)
+    ceo.add_child(hr_head)
+
+    return ceo
 
 if __name__ == '__main__':
     root_node = build_management_tree()
