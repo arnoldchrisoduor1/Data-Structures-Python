33c3978d92226c39268817cc8df7aef98584346b
Arnold Chris <arnoldchrisoduor@gmail.com>
2023-10-31 12:26:43 +0300

    initial binary tree

---
+++ Trees/binary_search_tree.py
@@ -0,0 +1,26 @@
+class BinarySearchTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def add_child(self, data):
+        if data == self.data:
+            return
+        if data < self.data:
+            #adding data in the left sub-tree.
+            if self.left:
+                self.left.add_child(data)
+            else:
+                self.left = BinarySearchTreeNode(data)
+        else:
+            if self.right:
+                self.right.add_child(data)
+            else:
+                self.right = BinarySearchTreeNode(data)
+    
+    def in_order_transversal(self):
+        elements = []
+
+
+        return elements
